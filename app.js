bed3dc84b8e526c67aab060eca05ae87359e2992
Mehmet Can Kahveci <96494651+mehmetcankahveci@users.noreply.github.com>
2023-01-07 00:34:19 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+let cookieValue = document.cookie
+  ?.split('=')[1];
+let x = cookieValue;
+if (cookieValue == null) {
+    document.cookie = "value = 0;"
+    location.reload();
+}
+document.getElementById('count').innerHTML = x;
+function increase() {
+   document.getElementById('count').innerHTML = ++x;
+   document.cookie = "value = " + x + ";"
+   location.reload();
+}
+function decrease() {
+    document.getElementById('count').innerHTML = --x
+    document.cookie = "value = " + x + ";"
+    location.reload();
+}
+function reset() {
+    document.cookie = "value = 0;"
+    location.reload();
+}
+function neunundsechzig() {
+    window.open("https://youtu.be/A1UhABiAfVk", '_blank');
+}
+if (x == 69) {
+    document.getElementById('sixtyninelol').style = "display = flex;"
+}
+if (x > 0) {
+    document.getElementById('count').style = "color: rgb(0, 181, 0);"
+}
+else if (x < 0) {
+    document.getElementById('count').style = "color: red;"
+}
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+        increase()
+    }
+    else if (e.keyCode == '40') {
+        decrease()
+    }
+    else if (e.keyCode == '37') {
+        decrease()
+    }
+    else if (e.keyCode == '39') {
+       increase()
+    }
+
+}
+document.addEventListener('keyup', event => {
+    if (event.code === 'Space') {
+      increase()
+    }
+  })
+
+
+
