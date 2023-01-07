d6364d61c2cebd99dad555d5a0ae4c2ab875926e
Mehmet Can Kahveci <96494651+mehmetcankahveci@users.noreply.github.com>
2023-01-07 16:56:28 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 let cookieValue = document.cookie
   ?.split('=')[1];
 let x = cookieValue;
+
 if (cookieValue == null) {
     document.cookie = "value = 0;"
-    window.location.reload();
+    window.location = window.location
 }
 document.getElementById('count').innerHTML = x;
 function increase() {
    document.getElementById('count').innerHTML = ++x;
    document.cookie = "value = " + x + ";"
-   window.location.reload();
+   location.reload()
 }
 function decrease() {
     document.getElementById('count').innerHTML = --x
     document.cookie = "value = " + x + ";"
-    window.location.reload();
+    location.reload()
 }
 function reset() {
     document.cookie = "value = 0;"
-    window.location.reload();
+    location.reload()
 }
 function neunundsechzig() {
     window.open("https://youtu.be/A1UhABiAfVk", '_blank');
@@ -29,9 +30,11 @@ if (x == 69) {
 if (x > 0) {
     document.getElementById('count').style = "color: rgb(0, 181, 0);"
 }
+
 else if (x < 0) {
     document.getElementById('count').style = "color: red;"
 }
+
 document.onkeydown = checkKey;
 
 function checkKey(e) {
@@ -57,6 +60,7 @@ document.addEventListener('keyup', event => {
       increase()
     }
   })
+
 
 
 
